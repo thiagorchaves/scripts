3692c51fe27b164ab72b9fdf6b11f9b6fe780a4f
thiagorchaves <thiago@qcx.com.br>
2018-11-19 14:54:18 -0200

    Desligando ec2 pela Tag Name

--- desligar_ec2.sh
+++ desligar_ec2.sh
@@ -1,19 +1,13 @@
 #!/bin/bash
 
 #Pega a instancia pela tag nome
-id_instancia=$(aws ec2 describe-instances --filters "Name=instance-id,Values=$1" | grep $1 | awk '{ print $2 }' | tr -d '"' | tr -d ',')
-
-nome_instancia=$(aws ec2 describe-instances --filters "Name=tag:Name,Values=$2" | grep $2 | awk '{ print $2 }' | tr -d '"' | tr -d ',')
- 
-
-# Cria a imagem
-aws ec2 create-image --instance-id $($id_instancia) --name "$1_$(date +%F)" --description "$1" --no-reboot
+nome_instancia=$(aws ec2 describe-instances --filters "Name=tag:Name,Values=$1" --query 'Reservations[].Instances[].InstanceId' | tr -d '"' | tr -d '[' | tr -d ']')
 
 if [ $? -eq 0 ]
 then
 # Desliga a instancia
  
-    aws ec2 stop-instances --instance-ids $($id_instancia)
+    aws ec2 stop-instances --instance-ids $nome_instancia
 else
     echo "Favor colocar o id da instância"
 fi
